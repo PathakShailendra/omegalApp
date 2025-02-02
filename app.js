8229451eac781dcddf2005694982361040a677cd
Shailendra pathak <pathakharsh813@gmail.com>
2025-02-03 00:04:13 +0530

    Application completed successfully

--- app.js
+++ app.js
@@ -1,49 +1,66 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const indexRouter = require('./routes/index');
+const path = require("path");
+const indexRouter = require("./routes/index");
 
-const http = require('http');
-const socketIO = require('socket.io');
+const http = require("http");
+const socketIO = require("socket.io");
 const server = http.createServer(app);
 const io = socketIO(server);
 
 let waitingusers = [];
 let rooms = {};
 
-io.on('connection', (socket) => {
-    socket.on('joinroom', () => {
-        if (waitingusers.length > 0) {
-            let partner = waitingusers.shift();
-            const roomname = `${socket.id}-${partner.id}`;
-            socket.join(roomname);
-            partner.join(roomname);
+io.on("connection", (socket) => {
+  socket.on("joinroom", () => {
+    if (waitingusers.length > 0) {
+      let partner = waitingusers.shift();
+      const roomname = `${socket.id}-${partner.id}`;
+      socket.join(roomname);
+      partner.join(roomname);
 
-            io.to(roomname).emit('joined', roomname);
-        } else {
-            waitingusers.push(socket);
-        }
-    })
+      io.to(roomname).emit("joined", roomname);
+    } else {
+      waitingusers.push(socket);
+    }
+  });
 
-    socket.on('message', (data) => {
-        socket.broadcast.to(data.room).emit('message', data.message);
-    })
+  socket.on("signalingMessage", (data) => {
+    socket.broadcast.to(data.room).emit("signalingMessage", data.message);
+  });
 
-    socket.on('disconnect', () => {
-        let index = waitingusers.findIndex((waitinguser) => waitinguser.id === socket.id);
-        waitingusers.splice(index, 1);
-    });
-        
+  socket.on("message", (data) => {
+    socket.broadcast.to(data.room).emit("message", data.message);
+  });
+
+  socket.on("startVideoCall", ({ room }) => {
+    socket.broadcast.to(room).emit("incomingCall");
+  });
+
+  socket.on("rejectCall", ({ room }) => {
+    socket.broadcast.to(room).emit("callRejected");
+  });
+
+  socket.on("acceptCall", ({ room }) => {
+    socket.broadcast.to(room).emit("callAccepted");
+  });
+
+  socket.on("disconnect", () => {
+    let index = waitingusers.findIndex(
+      (waitinguser) => waitinguser.id === socket.id
+    );
+    waitingusers.splice(index, 1);
+  });
 });
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
